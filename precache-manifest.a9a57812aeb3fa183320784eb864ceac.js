9c6e8e3a0526ba59dd79209219fbe5c073c4ab77
Taewapon.B <petchz2012@hotmail.com>
2019-12-29 05:05:00 +0700

    Updates

---
+++ precache-manifest.a9a57812aeb3fa183320784eb864ceac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76e4721caec3ab8e0f727b65b8d1512",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2a7b3fa4a503ae19382d",
+    "url": "/react-tutorial/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "005102a79be27a9b6592",
+    "url": "/react-tutorial/static/js/2.198a64bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.198a64bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a7b3fa4a503ae19382d",
+    "url": "/react-tutorial/static/js/main.5c13ed6a.chunk.js"
+  },
+  {
+    "revision": "8d76b84dd1ea32b60fde",
+    "url": "/react-tutorial/static/js/runtime-main.f01b29f8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  }
+]);
